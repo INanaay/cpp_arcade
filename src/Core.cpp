08f8aafcce381a964cc1d4e1f900395aa94bb9c9
Lebon Nathan <nathan.lebon@epitech.eu>
2018-03-14 14:32:20 +0100

    Core

--- src/Core.cpp
+++ src/Core.cpp
@@ -8,6 +8,7 @@
 #include <dlfcn.h>
 #include <iostream>
 #include "Core.hpp"
+#include <dirent.h>
 
 Core::Core(const char *nameLib)
 {
@@ -16,6 +17,7 @@ Core::Core(const char *nameLib)
 
 void	Core::start()
 {
+	createLibsTab();
 	m_lib->initWindow();
 	m_lib->drawMenu();
 	m_lib->destroyWindow();
@@ -36,4 +38,46 @@ void	Core::loadLib(const char *nameLib)
 		std::exit(84);
 	}
 	m_lib = create();
+}
+
+bool Core::endsWith(const std::string &str, const std::string &suffix) const
+{
+	return str.size() >= suffix.size() &&
+	       str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+void Core::createLibsTab()
+{
+	DIR *dpdf;
+	struct dirent *epdf;
+	std::string toAdd;
+
+	dpdf = opendir("lib");
+	if (dpdf == NULL)
+		throw std::exception_ptr();
+	while (epdf = readdir(dpdf))
+	{
+		toAdd = epdf->d_name;
+		if (endsWith(epdf->d_name, ".so")) {
+			m_libsTab.push_back(toAdd);
+		}
+
+	}
+	closedir(dpdf);
+}
+
+const std::vector<std::string> &Core::getM_libsTab() const {
+	return m_libsTab;
+}
+
+void Core::setM_libsTab(const std::vector<std::string> &m_libsTab) {
+	Core::m_libsTab = m_libsTab;
+}
+
+const std::vector<std::string> &Core::getM_gamesTab() const {
+	return m_gamesTab;
+}
+
+void Core::setM_gamesTab(const std::vector<std::string> &m_gamesTab) {
+	Core::m_gamesTab = m_gamesTab;
 }
--- src/Core.hpp
+++ src/Core.hpp
@@ -17,8 +17,17 @@ public:
 	~Core() = default;
 	void	loadLib(const char *);
 	void	start();
+	const std::vector<std::string> &getM_libsTab() const;
+	void setM_libsTab(const std::vector<std::string> &m_libsTab);
+	const std::vector<std::string> &getM_gamesTab() const;
+	void setM_gamesTab(const std::vector<std::string> &m_gamesTab);
+	void createLibsTab();
+	void createGamesTab();
 private:
+	bool endsWith(const std::string &str, const std::string &suffix) const;
 	IGlib	*m_lib;
+    	std::vector<std::string> m_libsTab;
+    	std::vector<std::string> m_gamesTab;
 };
 
 #endif //CORE_HPP
