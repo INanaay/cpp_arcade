d5d7c4246ce05be11b3b14c2aea2214128c1deb5
leBovin <flavian.gontier@epitech.eu>
2018-03-26 19:34:44 +0200

    Bigger map added

---
+++ inc/core/GameException.hpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** Epitech scolarship project (4 years remaining)
+** File description:
+**      Made on 2018/03 by lebovin
+*/
+
+#ifndef CPP_ARCADE_GAMEEXCEPTION_HPP
+#define CPP_ARCADE_GAMEEXCEPTION_HPP
+
+#endif //CPP_ARCADE_GAMEEXCEPTION_HPP
