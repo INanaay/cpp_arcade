dc13f8ebb85b62071aee5812d6f0bc3eed0b1a97
leBovin <flavian.gontier@epitech.eu>
2018-03-26 19:34:59 +0200

    GameException added

--- inc/core/GameException.hpp
+++ inc/core/GameException.hpp
@@ -8,4 +8,22 @@
 #ifndef CPP_ARCADE_GAMEEXCEPTION_HPP
 #define CPP_ARCADE_GAMEEXCEPTION_HPP
 
+#include <string>
+#include <exception>
+
+class GameException : public std::exception
+{
+public:
+	explicit GameException(const std::string &message) : m_message(message)
+	{
+
+	}
+	const char *what() const noexcept override
+	{
+		return m_message.c_str();
+	}
+private:
+	std::string m_message;
+};
+
 #endif //CPP_ARCADE_GAMEEXCEPTION_HPP
