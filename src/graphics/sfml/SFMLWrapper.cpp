6a1d955102ce03333ae2e47ec1d82c0414d2cbb0
Lebon Nathan <nathan.lebon@epitech.eu>
2018-04-05 21:22:07 +0200

    Fixed smlf sprite position

--- src/graphics/sfml/SFMLWrapper.cpp
+++ src/graphics/sfml/SFMLWrapper.cpp
@@ -176,8 +176,8 @@ void SFMLWrapper::drawEntity(const Entity &entity)
 	else
 		texture = m_cache[entity.sprite];
 	sprite = sf::Sprite(texture);
-	sprite.setPosition(entity.screenPosition.first,
-			entity.screenPosition.second);
+	sprite.setPosition(entity.screenPosition.first + 15,
+			entity.screenPosition.second + 15);
 	sprite.setOrigin(15, 15);
 	sprite.setRotation(90 * (int) entity.direction);
 	m_win->draw(sprite);
